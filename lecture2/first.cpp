6db07af19e41999008d169aa5cfdc8f9921374d1
Khushi Jadon <khushijadon@Khushis-MacBook-Air.local>
2024-03-12 22:09:09 +0530

    lecture5 update
    Signed-off-by: Khushi Jadon <khushijadon@Khushis-MacBook-Air.local>

---
+++ lecture2/first.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //cout << "Hello World!";
+ // int a,b;
+  //cin>>a;
+
+  //cout <<"value of a is:"<<a<<endl;
+ /* if (a>0){
+    cout<<"a is positive"<<endl;
+  }
+  else{
+    cout <<"a is negative"<<endl;
+  }
+
+  return 0;
+*/
+int a;
+cin>>a;
+
+cout <<"value of a is:"<<a<<endl;
+if (a>0){
+  cout <<"a is postive"<<endl;
+}
+else if(a<0){
+  cout<<"a is negative"<<endl;
+}
+else{
+  cout<<"a is 0"<<endl;
+}
+}
